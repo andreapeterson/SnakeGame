949bafd79f9653ce1b966994b2667ac7ee468345
andreapeterson <134665743+andreapeterson@users.noreply.github.com>
2023-07-09 14:33:04 -0400

    Update gameover.py

--- gameover.py
+++ gameover.py
@@ -1,19 +1,19 @@
-from turtle import Turtle
-from snake import Snake
+# from turtle import Turtle
+# from snake import Snake
 
 
-class Gameover(Turtle, Snake):
-    """Will display Game over in the middle of the screen when called"""
+# class Gameover(Turtle, Snake):
+#     """Will display Game over in the middle of the screen when called"""
 
-    def __init__(self):
-        super().__init__()
-        self.hideturtle()
+#     def __init__(self):
+#         super().__init__()
+#         self.hideturtle()
 
-    def end_game(self):
-        self.color("white")
-        self.penup()
-        self.goto(0, 0)
-        self.hideturtle()
-        self.write("Game over.", align="center", font=("Courier", 25, "normal"))
+#     def end_game(self):
+#         self.color("white")
+#         self.penup()
+#         self.goto(0, 0)
+#         self.hideturtle()
+#         self.write("Game over.", align="center", font=("Courier", 25, "normal"))
 
 
